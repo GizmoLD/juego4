457c68a78f66a5a83c87cba9d38ee3dbd81655cb
GizmoLD <122391569+GizmoLD@users.noreply.github.com>
2024-05-15 02:59:06 +0200

    1

--- core/src/com/mygdx/game4/MyGdxGame4.java
+++ core/src/com/mygdx/game4/MyGdxGame4.java
@@ -4,40 +4,58 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Net;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.net.HttpRequestBuilder;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.utils.Timer;
 
 public class MyGdxGame4 extends ApplicationAdapter {
-	Dialog endDialog;
+	private static final String SKIN_FILE_PATH = "clean-crispy-ui.json";
+	private static final String BUTTON_LABEL = "Send HTTP Request";
+	private static final String HTTP_REQUEST_URL = "https://www.iesesteveterradas.cat/";
 
-	Skin skin;
-	Stage stage;
+	private Skin skin;
+	private Stage stage;
+	private Dialog endDialog;
 
 	@Override
 	public void create() {
-		skin = new Skin(Gdx.files.internal("clean-crispy-ui.json"));
-
+		skin = new Skin(Gdx.files.internal(SKIN_FILE_PATH));
 		stage = new Stage();
-
 		Gdx.input.setInputProcessor(stage);
 
-		endDialog = new Dialog("End Game", skin) {
+		setupDialog();
+	}
+
+	private void setupDialog() {
+		BitmapFont font = new BitmapFont();
+		font.getData().setScale(2);
+
+		// Crea una nueva etiqueta con la fuente aumentada
+		Label.LabelStyle labelStyle = new Label.LabelStyle();
+		labelStyle.font = font;
+
+		Label label = new Label("HTTP Request Sent", labelStyle);
+
+		endDialog = new Dialog("", skin) {
 			protected void result(Object object) {
 				if (object.equals("solicitud_http")) {
-					realizarSolicitudHTTP();
+					sendHttpRequest();
 				}
 			}
 		};
 
-		endDialog.button("Solicitud HTTP", "solicitud_http");
+		endDialog.getContentTable().add(label);
+		endDialog.button(BUTTON_LABEL, "solicitud_http");
+		endDialog.show(stage);
 	}
 
-	private void realizarSolicitudHTTP() {
+	private void sendHttpRequest() {
 		HttpRequestBuilder requestBuilder = new HttpRequestBuilder();
-		Net.HttpRequest httpRequest = requestBuilder.newRequest().method(Net.HttpMethods.GET).url("https://www.iesesteveterradas.cat/").build();
+		Net.HttpRequest httpRequest = requestBuilder.newRequest().method(Net.HttpMethods.GET).url(HTTP_REQUEST_URL).build();
+
 		Gdx.net.sendHttpRequest(httpRequest, new Net.HttpResponseListener() {
 			@Override
 			public void cancelled() {
@@ -51,28 +69,40 @@ public class MyGdxGame4 extends ApplicationAdapter {
 
 			@Override
 			public void handleHttpResponse(Net.HttpResponse httpResponse) {
-				System.out.println("REBUT!");
+				System.out.println("HTTP RESPONSE RECEIVED");
+				Gdx.app.postRunnable(new Runnable() {
+					@Override
+					public void run() {
+						showDialog("HTTP Request Sent");
+					}
+				});
 			}
 		});
 
-		System.out.println("Solicitud HTTP enviada");
+		System.out.println("HTTP Request Sent");
+	}
+
+	private void showDialog(String message) {
+		Label label = new Label(message, skin.get("default", Label.LabelStyle.class));
+		label.setFontScale(2);
+
+		endDialog.getContentTable().clear();
+		endDialog.getContentTable().add(label);
+		endDialog.show(stage);
 	}
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(0, 1, 0, 1); // Establece el color de fondo a verde
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Borra el buffer de color con el color de fondo especificado
+		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		stage.act(); // Actualiza todos los actores de la escena
-		stage.draw(); // Dibuja la escena
-
-		if (!endDialog.hasParent()) {
-			endDialog.show(stage);
-		}
+		stage.act();
+		stage.draw();
 	}
 
 	@Override
 	public void dispose() {
 		stage.dispose();
+		skin.dispose();
 	}
 }
